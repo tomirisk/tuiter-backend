2e641581bff7f0d54d3c9513b586a024bad77fc2
sghosalkar <shubhamghosalkar.sg@gmail.com>
2022-03-24 22:09:50 -0400

    Added stats property in tuit model

--- models/tuits/tuit.ts
+++ models/tuits/tuit.ts
@@ -2,9 +2,11 @@
  * @file Implements model to represent tuits.
  */
 import User from "../users/user";
+import Stats from "./stats";
 
 export default interface Tuit {
     tuit: string,
     postedBy: User,
-    postedOn?: Date
+    postedOn?: Date,
+    stats: Stats
 };
